139e6e9f16ecd4e37863f8f5791aa9fe2b17cea2
Andy Bell <ambell@ucdavis.edu>
2015-03-03 13:22:40 -0800

    rm problem comment from animated_gif.py

--- animated_gif.py
+++ animated_gif.py
@@ -26,35 +26,6 @@ def animated_gif(folder_with_images, gif_filename, loop_duration, size):
 
 	output_file = os.path.join(folder_with_images, gif_filename)   # path for output file
 	writeGif(output_file, images, png_time, repeat=True, dither=False, nq=10)  # writes out GIF
-	
-	# def writeGif(filename, images, duration=0.1, repeat=True, dither=False, nq=0):
-    """ writeGif(filename, images, duration=0.1, repeat=True, dither=False)
-
-    Write an animated gif from the specified images.
-
-    Parameters
-    ----------
-    filename : string
-       The name of the file to write the image to.
-    images : list
-        Should be a list consisting of PIL images or numpy arrays.
-        The latter should be between 0 and 255 for integer types, and
-        between 0 and 1 for float types.
-    duration : scalar or list of scalars
-        The duration for all frames, or (if a list) for each frame.
-    repeat : bool or integer
-        The amount of loops. If True, loops infinitetely.
-    dither : bool
-        Whether to apply dithering
-    nq : integer
-        If nonzero, applies the NeuQuant quantization algorithm to create
-        the color palette. This algorithm is superior, but slower than
-        the standard PIL algorithm. The value of nq is the quality
-        parameter. 1 represents the best quality. 10 is in general a
-        good tradeoff between quality and speed.
-
-    """
-
 
 if __name__ == '__main__':
 	animated_gif()
